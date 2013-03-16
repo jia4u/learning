da4dbcaf615ad67877e9dd2f78a92cb26eabb936
jia4u <kongl123@163.com>
2013-03-16 14:04:56 +0800

    Initial Commit

---
+++ learning/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  learning
+//
+//  Created by losor on 3/16/13.
+//  Copyright (c) 2013 losor. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
